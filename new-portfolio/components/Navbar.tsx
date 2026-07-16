c3b424afd7a39e5aba22f3b5e2f02d46b6fbaf86
VINAY-YALAMANCHI <vyalamanchi@rivier.edu>
2026-07-16 07:33:25 -0700

    Added resume link to portfolio

--- new-portfolio/components/Navbar.tsx
+++ new-portfolio/components/Navbar.tsx
@@ -1,15 +1,22 @@
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <a className="logo" href="#home">
+      <a href="#home" className="logo">
         Vinay Yalamanchi
       </a>
 
       <div className="nav-links">
-        <a href="#projects">Work</a>
         <a href="#about">About</a>
         <a href="#experience">Experience</a>
+        <a href="#projects">Projects</a>
         <a href="#skills">Skills</a>
+        <a
+          href="/Vinay_Yalamanchi_Resume.pdf"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Resume
+        </a>
         <a href="#contact">Contact</a>
       </div>
     </nav>
